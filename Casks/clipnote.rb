044d5a3961ffe6da7c21042df9b0e41697d33422
hanhang3 <hanhang3@staff.weibo.cn>
2026-05-25 22:27:31 +0800

    fix: point cask to public release repo

--- Casks/clipnote.rb
+++ Casks/clipnote.rb
@@ -2,7 +2,7 @@ cask "clipnote" do
   version "1.1.0"
   sha256 "73611bc503ed9087209b41def63d9221f00deb0ff385e4c7b1e5b501173e4573"
 
-  url "https://github.com/hanhang-han/clipnote/releases/download/v#{version}/灵剪.dmg"
+  url "https://github.com/hanhang-han/clipnote-release/releases/download/v#{version}/灵剪.dmg"
   name "ClipNote"
   name "灵剪"
   desc "macOS 灵动岛剪贴板管理工具"
