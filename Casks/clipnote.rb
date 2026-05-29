429ed9b888256e7528324dacb7ec91bab2532999
hanhang3 <hanhang3@staff.weibo.cn>
2026-05-29 15:14:59 +0800

    bump clipnote to v1.3.3

--- Casks/clipnote.rb
+++ Casks/clipnote.rb
@@ -1,6 +1,6 @@
 cask "clipnote" do
-  version "1.3.2"
-  sha256 "f56d23ebc3f77591f3982c6890966b0f6410e1bb4f2d83f878f3ea7a7a3d121a"
+  version "1.3.3"
+  sha256 "6c9270015871e4ab5d6779a3b6ff0e0477138b579699c316e5eaa121ade403c4"
 
   url "https://github.com/hanhang-han/clipnote-release/releases/download/v#{version}/灵剪.dmg"
   name "ClipNote"
