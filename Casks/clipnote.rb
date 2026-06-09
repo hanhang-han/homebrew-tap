4f238c26a198214b3688b0a9fc381cf9b0b2d576
hanhang3 <hanhang3@staff.weibo.cn>
2026-06-09 15:46:11 +0800

    update: v3.2.1 macOS 14+ Intel/Apple Universal

--- Casks/clipnote.rb
+++ Casks/clipnote.rb
@@ -1,6 +1,6 @@
 cask "clipnote" do
   version "3.2.1"
-  sha256 "27fbc8e6a0f9e991c4b3190c4dffc0cfa401a87c7b7c7eef6b79a57bc0e4cb77"
+  sha256 "bc471d4f9cba73e8bd136ffb4b6a4ebff85e289d45f5341c54a4659b621ebcf0"
 
   url "https://github.com/hanhang-han/clipnote/releases/download/v#{version}/%E7%81%B5%E5%89%AA.dmg"
   name "ClipNote"
